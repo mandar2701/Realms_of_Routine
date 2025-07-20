47322e9dee4207a13a3f9b01ce21e2632ba5b618
mandar2701 <mandarphutane27@gmail.com>
2025-07-20 12:50:24 +0530

    Created Responsive Container in todo_screen.dart

--- Frontend/lib/pages/todo_screen.dart
+++ Frontend/lib/pages/todo_screen.dart
@@ -5,12 +5,28 @@ class TodoScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Stack(
         children: [
           // Background
           Positioned.fill(
-            child: Image.asset('assets/todo_background.png', fit: BoxFit.cover),
+            child: Image.asset('assets/todo_background.png', fit: BoxFit.fill),
+          ),
+
+          // Foreground Container
+          Center(
+            child: Container(
+              width: screenWidth * 0.90, // 90% of screen width
+              height: screenHeight * 0.7,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                //  color: Colors.white.withOpacity(0.9),
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(color: Colors.orange, width: 2),
+              ),
+            ),
           ),
         ],
       ),
