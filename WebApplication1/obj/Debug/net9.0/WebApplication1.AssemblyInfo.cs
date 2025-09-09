8af89d50ebdea6b591393c649b5dfec5a5473533
mandar2701 <mandarphutane27@gmail.com>
2025-09-10 05:01:22 +0530

    Add character assets and update pubspec for Font Awesome integration

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a5a55511087e32b6ccb11250f08bd6a0af99d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd225496b64ec50eef814320bd81fa95f4b1150")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
