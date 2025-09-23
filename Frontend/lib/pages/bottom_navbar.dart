5d59ae8391f790ee7452743d1b53f451b3360d96
Raphael <raphael.kv05@gmail.com>
2025-09-23 14:12:20 +0530

    Refactor: Update initial route and add blur effect to BottomNavBar
    - Modified `main.dart` to consistently show `HomeScreen` as the initial route, regardless of user authentication state.
    - In `bottom_navbar.dart`, wrapped the `Container` with `ClipRRect` and `BackdropFilter` to apply a blur effect to the bottom navigation bar.

--- Frontend/lib/pages/bottom_navbar.dart
+++ Frontend/lib/pages/bottom_navbar.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'game.dart';
@@ -11,72 +13,80 @@ class BottomNavbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 5),
-      decoration: BoxDecoration(
-        color: Colors.white24,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          // Home icon (currently no navigation)
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomeScreen()),
-              );
-            },
-            child: Image.asset('assets/icons/home.png', width: 50),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 5),
+          decoration: BoxDecoration(
+            color: Colors.white24,
+            borderRadius: BorderRadius.circular(12),
           ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              // Home icon (currently no navigation)
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomeScreen()),
+                  );
+                },
+                child: Image.asset('assets/icons/home.png', width: 50),
+              ),
 
-          // TodoScreen
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const TodoScreen()),
-              );
-            },
-            child: Image.asset('assets/icons/todo.png', width: 60),
-          ),
+              // TodoScreen
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const TodoScreen()),
+                  );
+                },
+                child: Image.asset('assets/icons/todo.png', width: 60),
+              ),
 
-          // ProfileScreen
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ProfileScreen()),
-              );
-            },
-            child: Image.asset('assets/icons/profile.png', width: 70),
-          ),
+              // ProfileScreen
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ProfileScreen(),
+                    ),
+                  );
+                },
+                child: Image.asset('assets/icons/profile.png', width: 70),
+              ),
 
-          // QuestCalendarScreen
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const QuestCalendarScreen(),
-                ),
-              );
-            },
-            child: Image.asset('assets/icons/calender.png', width: 50),
-          ),
+              // QuestCalendarScreen
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const QuestCalendarScreen(),
+                    ),
+                  );
+                },
+                child: Image.asset('assets/icons/calender.png', width: 50),
+              ),
 
-          // GameScreen
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const GameScreen()),
-              );
-            },
-            child: Image.asset('assets/icons/ai.png', width: 50),
+              // GameScreen
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const GameScreen()),
+                  );
+                },
+                child: Image.asset('assets/icons/ai.png', width: 50),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
